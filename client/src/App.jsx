25f52cf3dcf389ad17dede00129a27e484e24f92
Swaroop-Acharya <swaroopacharya935@gmail.com>
2025-08-05 21:34:52 +0530

    impl: added listners for loading messages, joined room

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,16 +7,41 @@ const socket = io("http://localhost:8000");
 function App() {
   const [roomCode, setRoomCode] = useState("");
   const [name, setName] = useState("");
-
+  const [messages, setMessages] = useState([]);
+  const [msgText, setMsgText] = useState("");
   useEffect(() => {
     socket.on("room-created", (code) => {
       setRoomCode(code);
       console.log(code);
     });
+
+    socket.on("joined-room", ({ code, messages }) => {
+      setRoomCode(code);
+      setMessages(messages);
+    });
+
+    socket.on("new-message", (message) => {
+      setMessages((prev) => [...prev, message]);
+    });
+
+    return () => {
+      socket.off();
+    };
   }, []);
 
-  const createRoom = () => {
+  const handleCreateRoom = () => {
     socket.emit("create-room");
+  };
+
+  const handleJoinRoom = () => {
+    if (!roomCode) return alert("Enter room ID");
+    socket.emit("join-room", { roomCode });
+  };
+
+  const handleSendMessage = () => {
+    if (!messages.trim()) return;
+    socket.emit("send-message", { roomCode, name, text: msgText.trim() });
+    setMsgText("");
   };
   return (
     <>
@@ -28,9 +53,29 @@ function App() {
         onChange={(e) => setName(e.target.value)}
       />
 
-      <button type="button" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer" onClick={createRoom}>
+      <button
+        type="button"
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer"
+        onClick={handleCreateRoom}
+      >
         Create Realm
       </button>
+
+      <input
+        type="text"
+        name="RoomCode"
+        placeholder="Room Code"
+        onChange={(e) => setRoomCode(e.target.value)}
+      />
+
+      <button
+        type="button"
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer"
+        onClick={handleJoinRoom}
+      >
+        Join Realm
+      </button>
+
       <h4>Room code: {roomCode}</h4>
     </>
   );
